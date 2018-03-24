e12f375f172c1741687bb3162f48f09ad5e3a61a
longdog <linda@longdog.us>
2018-03-24 12:42:24 +0400

    add polyfills

---
+++ client/polyfils.js
@@ -0,0 +1,8 @@
+// import 'event-source-polyfill';
+import from from "core-js/library/fn/array/from";
+import assign from "core-js/library/fn/object/assign";
+import startsWith from "core-js/library/fn/string/virtual/starts-with";
+
+Array.from = from;
+Object.assign = assign;
+String.prototype.startsWith = startsWith;
